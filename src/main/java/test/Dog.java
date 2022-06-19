77548fd784077db03efd5c8d397d559714a73185
zjc <zjc@163.com>
2022-06-19 18:07:46 +0800

    push commit

--- src/main/java/test/Dog.java
+++ src/main/java/test/Dog.java
@@ -8,5 +8,6 @@ public class Dog {
         System.out.println("你好张建超4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
